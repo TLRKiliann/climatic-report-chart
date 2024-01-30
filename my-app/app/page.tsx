61fd617b9f9c8b538924dabe9795a1288b986eec
TLRKiliann <philogenie@protonmail.com>
2024-01-30 16:16:12 +0100

    data interaction at work

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -61,76 +61,88 @@ export default function App() {
   const labels: string[] = ["janvier", "février", "mars", "avril", "mai", "juin", 
     "juillet", "aout", "septembre", "octobre", "novembre", "decembre"];
 
+  const newData: number[] = [-3, -1, 2, 9, 10, 16, 20, 17, 13, 8, 3, -2];
+  const newData2: number[] = [6, 7, 7, 13, 14, 17, 20, 20, 17, 11, 9, 5];
+  //const newData3: number[] = [4, 6, 8, 9, 14, 20, 22, 21, 19, 15, 7, 6];
+
+  const [dataInsert, setDataInsert] = useState<string>("")
+
+  const [newData3, setNewData3] = useState<number[]>([]);
+
+
   const dataTruck: DataProps = {
     labels,
     datasets: [
       {
         label: 'T°C 2010',
-        data: [-3, -1, 2, 9, 10, 16, 20, 17, 13, 8, 3, ],
+        data: newData,
         borderColor: 'rgb(53, 162, 235)',
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
       {
         label: 'T°C 2020',
-        data: [6, 7, 7, 13, 14, 17, 20, 20, 17, 11, 9, 5],
+        data: newData2,
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
       {
-        label: 'T°C 2023',
-        data: [4, 6, 8, 9, 14, 20, 22, 21, 19, 15, 7, 6],
+        label: 'T°C 2024',
+        data: newData3,
         borderColor: 'rgb(0,128,128)',
         backgroundColor: 'rgba(0,128,128, 0.5)',
       },
     ],
   };
 
-  const [dataLoad, setDataLoad] = useState<number[][]>([
-      dataTruck.datasets[0].data,
-      dataTruck.datasets[1].data,
-      dataTruck.datasets[2].data,
-    ]
-  );
-
-  const [dataInsert, setDataInsert] = useState<string>("")
-
-  const handleChangeData = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeData = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setDataInsert(event?.target.value)
   };
 
-  const handleClickData = () => {
-    //const mapping: number[] = dataLoad[0].concat(Number(dataInsert));
-    const mappData = dataLoad.map((d) => d === dataTruck.datasets[0].data ? {...d, dataInsert}: d);
-    console.log(mappData, "mapping");
-    setDataLoad(mappData);
+  const handleClickData = (): void => {
+    console.log(dataInsert, "dataInsert")
+    const addElement = [...newData3, Number(dataInsert)];
+    setNewData3(addElement);
+    setDataInsert("");
   };
-
+  
+  console.log(newData, "newData");
   return (
-      <main className='w-full h-screen bg-slate-50'>
+      <main className='w-full h-screen bg-white'>
         
-        <h1 className='text-3xl font-bold text-center pt-10 pb-14 text-slate-600'>
+        <h1 className='text-3xl font-bold text-center py-10 text-slate-700 drop-shadow-lg'>
           ChartJS
         </h1>
-      
+
+
         <div className='relative flex w-3/5 h-auto m-auto'>
           <Line ref={(reference) => reference} options={options} data={dataTruck} 
-            className='absolute w-3/5 max-w-3/5 bg-slate-100/50 py-1 rounded-lg shadow-md'/>
+            className='absolute w-3/5 max-w-3/5 bg-slate-50/40 py-1 rounded-lg shadow-boxchart'/>
         </div>
-      
-        <div className="relative w-full h-auto mt-[400px] border-2">
-          <input type="text" value={dataInsert} onChange={(e) => handleChangeData(e)} 
-            className='text-slate-100 bg-slate-800'
+
+
+        <div className="absolute top-[500px] xl:top-[700px] flex items-center justify-center w-full">
+
+          <label htmlFor="data" className='text-sm text-slate-600'>
+            Enter number of 2024:
+          </label>
+
+          <input type="text" id="data" name="data" value={dataInsert} onChange={(e) => handleChangeData(e)} 
+            className='text-slate-700 bg-slate-200 
+              border hover:border-blue-400 focus:border-blue-400 mx-4 px-2 py-1 rounded'
           />
+
           <button type="button" onClick={handleClickData}
-            className='text-slate-100 bg-slate-600 px-4 py-2 rounded'
+            className='font-bold text-slate-100 bg-sky-600 hover:bg-sky-700 hover:scale-95 
+            active:bg-sky-500 active:scale-105 px-6 py-1 rounded shadow-boxcustom hover:shadow-none'
           >
             Enter
           </button>
         </div>
 
+
         <div>
           <p className='text-slate-900'>
-            {dataLoad}
+            {newData}
           </p>
         </div>
 
