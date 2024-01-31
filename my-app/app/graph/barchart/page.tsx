1bddc97e683859ab789765f526d4b7856a8898dc
TLRKiliann <philogenie@protonmail.com>
2024-01-31 20:11:24 +0100

    code splitting

--- my-app/app/graph/barchart/page.tsx
+++ my-app/app/graph/barchart/page.tsx
@@ -1,84 +1,8 @@
-"use client";
-
 import React from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
 import Link from 'next/link';
+import BarChartComp from '@/app/components/BarChartComp';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-type DataProps = {
-  labels: string[];
-  datasets: {
-      label: string;
-      data: number[];
-      backgroundColor: string;
-  }[]
-};
-
-
-export default function BarChart() {
-
-  const newData: number[] = [-3, -1, 2, 9, 10, 16, 20, 17, 13, 8, 3, -2];
-  const newData2: number[] = [6, 7, 7, 13, 14, 17, 20, 20, 17, 11, 9, 5];
-  const newData3: number[] = [4, 6, 8, 9, 14, 20, 22, 21, 19, 15, 7, 6];
-  //const newDataBase3: number[] = [4, 6, 8, 9];
-
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'bottom' as const,
-      },
-      title: {
-        display: true,
-        text: 'Temperatures (celsius) in Lausanne 2010-2020',
-        font: {
-          size: 14,
-        },
-        color: "dimgrey",
-      },
-    },
-  };
-  
-  const labels: string[] = ["janvier", "février", "mars", "avril", "mai", "juin", 
-    "juillet", "aout", "septembre", "octobre", "novembre", "decembre"];
-
-  const data: DataProps = {
-    labels,
-    datasets: [
-      {
-        label: 'T°C 2010',
-        data: newData,
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'T°C 2020',
-        data: newData2,
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-      {
-        label: 'T°C 2024',
-        data: newData3,
-        backgroundColor: 'rgba(0,128,128, 0.5)',
-      },
-    ],
-  };
+export default async function BarChart() {
 
   return (
     <main className='w-full h-screen bg-white'>
@@ -90,12 +14,9 @@ export default function BarChart() {
       <h1 className='text-3xl font-bold text-center py-10 text-slate-700 drop-shadow-lg'>
         BarChart
       </h1>
-      
-      <div className='relative flex w-3/5 h-auto m-auto'>
-        <Bar options={options} data={data}
-          className='absolute w-3/5 max-w-3/5 bg-slate-50/40 px-2 py-1 rounded-lg shadow-boxchart' />
-      </div>
-    
+
+      <BarChartComp />
+
     </main>
   );
 }
--- my-app/app/graph/linechart/page.tsx
+++ my-app/app/graph/linechart/page.tsx
@@ -1,98 +1,42 @@
 "use client";
 
 import React, { useState } from 'react';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
 import Link from 'next/link';
+import LineChartComp from '@/app/components/LineChartComp';
+import { labels } from '@/app/lib/graph-config';
+import { DataProps } from '@/app/lib/definitions';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-type DataProps = {
-  labels: string[];
-  datasets: {
-      label: string;
-      data: number[];
-      borderColor: string;
-      backgroundColor: string;
-  }[]
-};
-
-export default function Graph() {
-
-    const options = {
-        responsive: true,
-        plugins: {
-        legend: {
-            position: 'bottom' as const,
-        },
-        title: {
-            display: true,
-            text: 'Temperatures (celsius) in Lausanne 2010-2020',
-            font: {
-                size: 14,
-            },
-            color: "dimgrey",
-        },
-        },
-        elements: {
-        point: {
-            radius: 5,
-            hitRadius: 10,
-        }
-        },
-    };
-
-    const labels: string[] = ["janvier", "février", "mars", "avril", "mai", "juin", 
-        "juillet", "aout", "septembre", "octobre", "novembre", "decembre"];
-
-    const newData: number[] = [-3, -1, 2, 9, 10, 16, 20, 17, 13, 8, 3, -2];
-    const newData2: number[] = [6, 7, 7, 13, 14, 17, 20, 20, 17, 11, 9, 5];
-    //const newDataBase3: number[] = [4, 6, 8, 9, 14, 20, 22, 21, 19, 15, 7, 6];
-    const newDataBase3: number[] = [4, 6, 8, 9];
+export default function LineChart() {
 
     const [dataInsert, setDataInsert] = useState<string>("")
 
-    const [newData3, setNewData3] = useState<number[]>(newDataBase3);
-
+    const newDataLine1: number[] = [-3, -1, 2, 9, 10, 16, 20, 17, 13, 8, 3, -2];
+    const newDataLine2: number[] = [6, 7, 7, 13, 14, 17, 20, 20, 17, 11, 9, 5];
+    const newDataBase3: number[] = [4, 6, 8, 9, 14,];
+    
+    const [changeData, setChangeData] = useState<number[]>(newDataBase3);
 
     const dataLine: DataProps = {
         labels,
         datasets: [
-        {
-            label: 'T°C 2010',
-            data: newData,
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-        },
-        {
-            label: 'T°C 2020',
-            data: newData2,
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
-        {
-            label: 'T°C 2024',
-            data: newData3,
-            borderColor: 'rgb(0,128,128)',
-            backgroundColor: 'rgba(0,128,128, 0.5)',
-        },
+            {
+                label: 'T°C 2010',
+                data: newDataLine1,
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+            {
+                label: 'T°C 2020',
+                data: newDataLine2,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'T°C 2024',
+                data: changeData,
+                borderColor: 'rgb(0,128,128)',
+                backgroundColor: 'rgba(0,128,128, 0.5)',
+            },
         ],
     };
 
@@ -101,17 +45,16 @@ export default function Graph() {
     };
 
     const handleClickData = (): void => {
-        //console.log(dataInsert, "dataInsert")
-        const addElement = [...newData3, Number(dataInsert)];
-        setNewData3(addElement);
+        const addElement = [...changeData, Number(dataInsert)];
+        setChangeData(addElement);
         setDataInsert("");
     };
     
     const handleDelete = (): void => {
-        const delLastNumber = newData3.splice(-1);
-        console.log(delLastNumber)
-        setNewData3(delLastNumber);
-    }
+        const copy = [...changeData]
+        copy.pop();
+        setChangeData(copy);
+    };
 
     //console.log(newData, "newData");
 
@@ -124,14 +67,10 @@ export default function Graph() {
                 LineChart
             </h1>
 
-
-            <div className='relative flex w-3/5 h-auto m-auto'>
-                <Line options={options} data={dataLine} 
-                    className='absolute w-3/5 max-w-3/5 bg-slate-50/40 py-1 rounded-lg shadow-boxchart'/>
-            </div>
+            <LineChartComp dataLine={dataLine} />
 
 
-            <div className="absolute top-[500px] xl:top-[700px] flex items-center justify-center w-full">
+            <form className="absolute top-[500px] xl:top-[700px] flex items-center justify-center w-full">
 
                 <label htmlFor="data" className='text-sm text-slate-600'>
                     Enter number of 2024:
@@ -162,13 +101,7 @@ export default function Graph() {
                     (Last one)
                 </legend>
 
-            </div>
-
-            <div>
-                <p className='text-slate-900'>
-                    {newData}
-                </p>
-            </div>
+            </form>
 
         </main>
     );
