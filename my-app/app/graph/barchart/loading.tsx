b5d424f35302fe025295aaa53d1f7119a3be34f9
TLRKiliann <philogenie@protonmail.com>
2024-01-31 20:10:33 +0100

    loading created

--- my-app/app/graph/barchart/loading.tsx
+++ my-app/app/graph/barchart/loading.tsx
@@ -2,12 +2,8 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className="w-full min-h-screen">
-        <div className='w-[200px] flex items-center p-4'>
-
-          <h2 className='font-bold ml-4'>Loading...</h2>
-
-        </div>
-    </div>
+    <h2 className='font-bold ml-4'>
+      Loading...
+    </h2>
   )
 }
